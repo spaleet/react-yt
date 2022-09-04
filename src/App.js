06479da49f9d12153bd411be7fd40da2a11fa5ef
Ali M <ali.mo.dev@gmail.com>
2022-09-04 20:10:56 +0430

    Navbar Design Complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Box from '@mui/material/Box';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components/_index";
 
 const App = () => (
     <BrowserRouter>
