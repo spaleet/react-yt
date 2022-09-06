db4e122173dffdaf0b786d9a9cb4deba1697a730
Ali M <ali.mo.dev@gmail.com>
2022-09-06 17:04:13 +0430

    finished VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -9,14 +9,29 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   const imgUrl = snippet?.thumbnails?.high?.url;
 
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={videoUrl}>
 
-        <CardMedia image={imgUrl} alt={snippet?.title} sx={{width: 358, height: 180}} />
-      
+        <CardMedia image={imgUrl} alt={snippet?.title} sx={{ width: 358, height: 180 }} />
+
       </Link>
-    
-      <CardContent sx={{backgroundColor: '#1e1e1e', height: "106px"}} />
+
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: "106px" }}>
+
+        <Link to={videoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title?.slice(0, 69)}
+          </Typography>
+        </Link>
+
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoDetails.channelUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle?.slice(0, 69)}
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+
+      </CardContent>
     </Card>
   )
 }
