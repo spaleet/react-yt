c4747c6c427f2ca218dbaad1f08ce53041c8b838
Ali M <ali.mo.dev@gmail.com>
2022-09-08 20:23:53 +0430

    UI fixes

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,10 +2,11 @@ import { useState, useReact } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
-import { CheckCircle, ThumbsUpDown } from '@mui/icons-material';
+import { CheckCircle } from '@mui/icons-material';
 import { Videos, Loader } from './_index';
 import { useEffect } from 'react';
 import { fetchRelatedVideos, fetchVideoDetail } from '../utils/apiCall';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 const VideoDetail = () => {
 
@@ -24,45 +25,56 @@ const VideoDetail = () => {
   const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
 
   return (
-    <Box minHeight="95vh">
-      <Stack direction={{ xs: "column", md: "row" }}>
-        
+    <Box minHeight="95vh" sx={{ pl: { md: '60px' } }}>
+      <Stack direction={{ xs: "column", md: "row" }} gap={20}>
+
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            
+
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
-            
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+
+            <Typography color="text.primary" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
 
-            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+            <Stack sx={{ direction: { xs: "column", md: "row" }, justifyContent: "space-between" }} py={1} px={2} >
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: "subtitle1", md: 'h6' }} color="#fff" >
+                <Typography variant={{ sm: "subtitle1", md: 'h6' }} color="text.primary" >
                   {channelTitle}
-                  <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                  <CheckCircle sx={{ fontSize: "12px", color: "text.primary", ml: "5px" }} />
                 </Typography>
               </Link>
 
-              <Stack direction="row" gap="20px" alignItems="center">
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+              <Stack direction="row" gap="20px" alignItems="center" color="text.secondary">
+
+                <Typography variant="body2">
                   {parseInt(viewCount).toLocaleString()} views
                 </Typography>
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+
+
+                <Typography variant="body2">
                   {parseInt(likeCount).toLocaleString()} likes
+                  <ThumbUpIcon sx={{ ml: '5px', fontSize: '20px', mb: '-5px' }} />
                 </Typography>
+
               </Stack>
 
             </Stack>
           </Box>
         </Box>
 
-        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
+        <Box sx={{
+          px: 2,
+          py: { md: 1, xs: 5 },
+          display: "flex",
+          justifyContent: "center", 
+          alignItems: "center", 
+        }}>
           <Videos videos={videos} direction="column" />
         </Box>
 
-      </Stack>
-    </Box>
+      </Stack >
+    </Box >
   )
 }
 
